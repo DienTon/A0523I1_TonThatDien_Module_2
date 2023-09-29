7c39deccb604af7e208f554a1ee8ed21b8282404
DienTon <132761242+DienTon@users.noreply.github.com>
2023-09-29 20:23:08 +0700

    update

--- bai_tap/src/com/codegym/ss15_exception/checkTriangle.java
+++ bai_tap/src/com/codegym/ss15_exception/checkTriangle.java
@@ -1,5 +1,12 @@
 package com.codegym.ss15_exception;
 
 public class checkTriangle {
-    
+    public void checkTriangle( int a, int b, int c) throws Exception{
+        if( a+b<=c||a + c <= b || b + c <= a){
+            throw new IllegalTriangleException("Loi: Tam giac khong hop le.");
+        }else {
+            System.out.println("Tam giac hop le.");
+        }
+
+    }
 }
--- bai_tap/src/com/codegym/ss15_exception/test.java
+++ bai_tap/src/com/codegym/ss15_exception/test.java
@@ -1,12 +1,26 @@
 package com.codegym.ss15_exception;
 
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        checkTriangle check= new checkTriangle() ;
+        int a,b,c;
         try {
-            return;
-        }
-        finally{
-            System.out.println( "Finally" );
+            System.out.println("Nhap canh a :");
+            a= scanner.nextInt();
+            System.out.println("Nhap canh b :");
+            b= scanner.nextInt();
+            System.out.println("Nhap canh c :");
+            c= scanner.nextInt();
+            try{
+                check.checkTriangle(a,b,c);
+            }catch (IllegalTriangleException e){
+                System.out.println(e.getMessage());
+            }
+        }catch (Exception e){
+            System.out.println("Khong dung dinh dang.");
         }
     }
 }
